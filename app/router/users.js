1bbba29d3b904db05178e307d68c659865c9a0fe
MatthieuSKRZYPCZAK <bibbou1@hotmail.com>
2023-02-21 12:26:55 +0100

    Added getUser, addUser, deleteUser

--- app/router/users.js
+++ app/router/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 router.get('/users', usersController.getAll);
 router.post('/register', usersController.addUser);
+router.get('/user/:id', usersController.getUser);
+router.patch('/user/:id', )
+router.delete('/user/:id', usersController.deleteUser);
 
 
 module.exports = router;
