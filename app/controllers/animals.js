32d28c27483c7fac9388fb52dd44b5626d4cb584
MatthieuSKRZYPCZAK <bibbou1@hotmail.com>
2023-02-22 14:58:55 +0100

    Updated script create_tables

--- app/controllers/animals.js
+++ app/controllers/animals.js
@@ -5,50 +5,6 @@ const multer = require('multer');
 
 const animalsController = {
 
-    async addAnimal(req, res, next) {
-        // Utiliser le middleware Multer pour gérer les téléchargements d'images d'animaux
-        const upload = multer({
-          storage: multer.diskStorage({
-            destination: (req, file, cb) => {
-              // Créer un répertoire pour l'animal s'il n'existe pas déjà
-              const animalDir = `uploads/${req.params.animalName}`;
-              if (!fs.existsSync(animalDir)) {
-                fs.mkdirSync(animalDir);
-              }
-              // Enregistrer l'image dans le répertoire de l'animal
-              cb(null, animalDir);
-            },
-            filename: (req, file, cb) => {
-              // Utiliser le nom d'origine de l'image
-              cb(null, file.originalname);
-            }
-          })
-        }).single('image');
-    
-        upload(req, res, async (err) => {
-          if (err instanceof multer.MulterError) {
-            return res.status(400).send('Une erreur est survenue lors du téléchargement de l\'image');
-          } else if (err) {
-            return res.status(400).send('Une erreur est survenue lors du téléchargement de l\'image');
-          }
-    
-          // Si l'animal a une image, ajouter le nom du fichier dans la base de données
-          const animalData = req.body;
-          if (req.file) {
-            animalData.image = req.file.filename;
-          }
-    
-          const animal = await Animal.create(animalData);
-          if (animal) {
-            res.json(animal);
-          } else {
-            next(new Error("Problème de BDD"));
-          }
-        });
-      },
-
-
-
     /**
      * Récupère la liste des animaux
      * @returns Liste des animaux
--- app/service/security.js
+++ app/service/security.js
@@ -7,16 +7,16 @@ const securityService = {
      * @param {*} res 
      * @param {*} next 
      */
-    isConnected(req, res, next) {
-        if (req.session.user) {
-            // user est présent, je continue mon chemin
-            next();
-        }
-        else {
-            // user est absent, je redirige vers la homepage
-            res.redirect("/");
-        }
-    },
+    // isConnected(req, res, next) {
+    //     if (req.session.user) {
+    //         // user est présent, je continue mon chemin
+    //         next();
+    //     }
+    //     else {
+    //         // user est absent, je redirige vers la homepage
+    //         res.redirect("/");
+    //     }
+    // },
     checkToken(req, res, next) {
         try {
             //console.log(req.headers.authorization.split(" ")[1]);
