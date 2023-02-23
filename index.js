5c22d43a59daec0ea7c1226c7f42c1f6833a65ab
MatthieuSKRZYPCZAK <bibbou1@hotmail.com>
2023-02-23 10:26:35 +0100

    updated

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
-
-
 const path = require('path');
 app.use(express.static(path.join(__dirname, './public')));
 app.use(cors())
