45b785bfe5dee82c57b390301a8e51cf60599203
roissi <cyrildegraeve@gmail.com>
2023-02-23 11:12:04 +0100

    swagger all routes sauf adopt

--- app/routers/animals.js
+++ app/routers/animals.js
@@ -2,10 +2,54 @@ const express = require('express');
 const { animalsController } = require('../controllers');
 const router = express.Router();
 
+/**
+ * GET /api/animals
+ * @summary Récupère tous les animals
+ * @tags ANIMAL
+ * @return {string} 200 - all animals
+ * @return {object} 500 - Unexpected error
+ */
+
 router.get('/animals', animalsController.getAll);
+
+/**
+ * POST /api/animals
+ * @summary Crée un animal
+ * @tags ANIMAL
+ * @return {string} 200 - new animal
+ * @return {object} 500 - Unexpected error
+ */
+
 router.post('/animal', animalsController.addAnimal);
+
+/**
+ * GET /api/animals/:id
+ * @summary Récupère un animal
+ * @tags ANIMAL
+ * @return {string} 200 - one animal
+ * @return {object} 500 - Unexpected error
+ */
+
 router.get('/animal/:id', animalsController.getAnimal);
+
+/**
+ * PATCH /api/animals/:id
+ * @summary Modifie un animal
+ * @tags ANIMAL
+ * @return {string} 200 - update animal
+ * @return {object} 500 - Unexpected error
+ */
+
 router.patch('/animal/:id', animalsController.updateAnimal);
+
+/**
+ * DELETE /api/animals/:id
+ * @summary Supprime un animal
+ * @tags ANIMAL
+ * @return {string} 200 - delete animal
+ * @return {object} 500 - Unexpected error
+ */
+
 router.delete('/animal/:id', animalsController.deleteAnimal);
 
 
--- app/routers/roles.js
+++ app/routers/roles.js
@@ -2,10 +2,54 @@ const express = require('express');
 const { rolesController } = require('../controllers');
 const router = express.Router();
 
+/**
+ * GET /api/roles
+ * @summary Récupère tous les roles
+ * @tags ROLE
+ * @return {string} 200 - all roles
+ * @return {object} 500 - Unexpected error
+ */
+
 router.get('/roles', rolesController.getAll);
+
+/**
+ * POST /api/roles
+ * @summary Crée un role
+ * @tags ROLE
+ * @return {string} 200 - new role
+ * @return {object} 500 - Unexpected error
+ */
+
 router.post('/role', rolesController.addRole);
+
+/**
+ * GET /api/roles/:id
+ * @summary Récupère un role
+ * @tags ROLE
+ * @return {string} 200 - one role
+ * @return {object} 500 - Unexpected error
+ */
+
 router.get('/role/:id', rolesController.getRole);
+
+/**
+ * PATCH /api/roles/:id
+ * @summary Modifie un role
+ * @tags ROLE
+ * @return {string} 200 - update role
+ * @return {object} 500 - Unexpected error
+ */
+
 router.patch('/role/:id', rolesController.updateRole);
+
+/**
+ * DELETE /api/roles/:id
+ * @summary Supprime un role
+ * @tags ROLE
+ * @return {string} 200 - delete role
+ * @return {object} 500 - Unexpected error
+ */
+
 router.delete('/role/:id', rolesController.deleteRole);
 
 
