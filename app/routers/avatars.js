34ed1287333b69b43aa576724d09a9e691f5396f
MatthieuSKRZYPCZAK <bibbou1@hotmail.com>
2023-02-24 13:21:46 +0100

    Updated route images

--- app/routers/avatars.js
+++ app/routers/avatars.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { avatarsController } = require('../controllers');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: 'public/images/'});
+const upload = multer({dest: 'public/images/avatars'});
 
 
 /**
