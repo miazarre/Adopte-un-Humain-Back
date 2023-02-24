09b33184a621ee185f82380a9a747fa4563615d6
MatthieuSKRZYPCZAK <bibbou1@hotmail.com>
2023-02-24 11:26:23 +0100

    Updated Joi schema

--- app/controllers/adopts.js
+++ app/controllers/adopts.js
@@ -7,51 +7,81 @@ const adoptsController = {
      * @returns Liste des adoptions
      */
     async getAll(_, res, next) {
-        const adopts = await Adopt.findAll();
-        if(adopts) {
-            res.json(adopts);
-        } else {
-            next(new Error("Problème de BDD"));
-        }   
+        try {
+            const adopts = await Adopt.findAll();
+            if(adopts) {
+                res.json(adopts);
+            } else {
+                next(new Error("Problème de BDD"));
+            }  
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
+        } 
     },
 
    // Récupère une adoption
     async getAdopt(req, res, next) {
-        const adopt = await Adopt.findByPk(req.params.id);
-        if(adopt) {
-            res.json(adopt);
-        } else {
-            next(new Error("Problème de BDD"));
+        try {
+            const adopt = await Adopt.findByPk(req.params.id);
+            if(adopt) {
+                res.json(adopt);
+            } else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
     // Ajoute une adoption
     async addAdopt(req, res, next) {
+        try{
           const addAdopt = await Adopt.create(req.body);
           if (addAdopt) {
             res.json(addAdopt);
           } else {
             next(new Error("Problème de BDD"));
-          }        
+          }  
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
+        }      
     },
 
     // Modifie une adoption
     async updateAdopt(req, res, next) {
-        const adopt = await Adopt.update(req.params.id, req.body);
-        if(adopt) {
-            res.json(adopt);
-        } else {
-            next(new Error("Problème de BDD"));
+        try {
+            const adopt = await Adopt.update(req.params.id, req.body);
+            if(adopt) {
+                res.json(adopt);
+            } else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
 
     // Supprime une adoption
     async deleteAdopt(req,res,next){
-        const adopt = await Adopt.delete(req.params.id);
-        if (adopt) {
-            res.json(adopt);
-        }
-        else {
-            next(new Error("Problème de BDD"));
+        try {
+            const adopt = await Adopt.delete(req.params.id);
+            if (adopt) {
+                res.json(adopt);
+            }
+            else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
 }
--- app/controllers/roles.js
+++ app/controllers/roles.js
@@ -8,61 +8,90 @@ const rolesController = {
      * @returns Liste des roles
      */
     async getAll(_, res, next) {
-        const roles = await Role.findAll();
-        if(roles) {
-            res.json(roles);
-        } else {
-            next(new Error("Problème de BDD"));
-        }   
+        try {
+            const roles = await Role.findAll();
+            if(roles) {
+                res.json(roles);
+            } else {
+                next(new Error("Problème de BDD"));
+            }   
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
+        }
     },
 
    // Récupère un role
     async getRole(req, res, next) {
-        const role = await Role.findByPk(req.params.id);
-        if(role) {
-            res.json(role);
-        } else {
-            next(new Error("Problème de BDD"));
+        try {
+            const role = await Role.findByPk(req.params.id);
+            if(role) {
+                res.json(role);
+            } else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
     // Ajoute un role
     async addRole(req, res, next) {
-
-        const role = new Role(req.body);
-        const roleExist = await role.checkRole();           // Controle si le role existe déjà
-        if (!roleExist) {
-          const addRole = await Role.create(req.body);
-          if (addRole) {
-            res.json(addRole);
-          } else {
-            next(new Error("Problème de BDD"));
-          }
-        } else {
-          // Le rôle existe déjà
-          res.status(500).json({
-            error: "Le rôle existe déjà !"
-          });
+        try {
+            const role = new Role(req.body);
+            const roleExist = await role.checkRole();           // Controle si le role existe déjà
+            if (!roleExist) {
+            const addRole = await Role.create(req.body);
+            if (addRole) {
+                res.json(addRole);
+            } else {
+                next(new Error("Problème de BDD"));
+            }
+            } else {
+            // Le rôle existe déjà
+            res.status(500).json({
+                error: "Le rôle existe déjà !"
+            });
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
 
     },
     // Modifie un role
     async updateRole(req, res, next) {
-        const role = await Role.update(req.params.id, req.body);
-        if(role) {
-            res.json(role);
-        } else {
-            next(new Error("Problème de BDD"));
+        try {
+            const role = await Role.update(req.params.id, req.body);
+            if(role) {
+                res.json(role);
+            } else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
 
     // Supprime un role
     async deleteRole(req,res,next){
-        const role = await Role.delete(req.params.id);
-        if (role) {
-            res.json(role);
-        }
-        else {
-            next(new Error("Problème de BDD"));
+        try {
+            const role = await Role.delete(req.params.id);
+            if (role) {
+                res.json(role);
+            }
+            else {
+                next(new Error("Problème de BDD"));
+            }
+        } catch(error) {
+            res.status(500).json({
+                error: "erreur !"
+            });
         }
     },
 }
