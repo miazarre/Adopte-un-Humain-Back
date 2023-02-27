3db6e4b520c99636597dad8b6137a822aed7b4c5
roissi <cyrildegraeve@gmail.com>
2023-02-27 17:18:39 +0100

    routes HAS

--- app/routers/photos.js
+++ app/routers/photos.js
@@ -11,3 +11,21 @@ router.get('/images/avatar/:filename', photosController.getPhotoAvatar);
 
 
 module.exports = router;
+
+// doc swagger : http://localhost:3000/api-docs
+
+/**
+ * GET /api/images/animal/:filename
+ * @summary Récupère toutes les photos des animaux
+ * @tags IMAGES
+ * @return {string} 200 - all animals photos
+ * @return {object} 500 - Unexpected error
+ */
+
+/**
+ * GET /api/avatar/:filename
+ * @summary Récupère toutes les illus des avatars
+ * @tags IMAGES
+ * @return {string} 200 - all avatars pictures
+ * @return {object} 500 - Unexpected error
+ */
