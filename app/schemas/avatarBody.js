ca6a823ba7b025ae5e72642ace8c82460eb3e6d2
roissi <cyrildegraeve@gmail.com>
2023-03-04 23:34:36 +0100

    import WIP

---
+++ app/schemas/avatarBody.js
@@ -0,0 +1,19 @@
+import Joi from 'joi';
+
+const avatarSchema = {
+  create() {
+    return Joi.object({
+      name: Joi.string(),
+      picture: Joi.binary()
+    });
+  },
+
+  update() {
+    return Joi.object({
+      name: Joi.string(),
+      priority: Joi.binary(),
+    });
+  },
+}
+
+export default avatarSchema;
