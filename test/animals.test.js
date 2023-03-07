ea5c1f3ce8c802e94f88379bd124279cfad226b4
roissi <cyrildegraeve@gmail.com>
2023-03-07 10:29:42 +0100

    TU mocha/chai  WIP

--- test/animals.test.js
+++ test/animals.test.js
@@ -2,48 +2,69 @@ import chai from 'chai';
 import chaiHttp from 'chai-http';
 import fs from 'fs';
 import path from 'path';
+import sinon from 'sinon';
+import Animal from "../app/models/animal.js"
 
 chai.use(chaiHttp);
 const expect = chai.expect;
 
-describe('POST /api/animals', () => {
-  it('should add an animal', function(done) {
-    this.timeout(30000);
+describe('getAll', () => {
+  it('should return all animals', async () => {
     // Arrange
-    const animalData = {
-      name: 'Test Animal',
-      description: 'This is a test animal',
-      needs: 'Test needs',
-      status: 'available',
-      resume: 'Test resume',
-      birthdate: '2021-01-01',
-    };
-    const filePath = path.join(new URL('.', import.meta.url).pathname, 'test_image.jpg');
+    const fakeAnimals = [{ name: 'Animal 1' }, { name: 'Animal 2' }];
+    const findAllStub = sinon.stub(Animal, 'findAll').resolves(fakeAnimals);
+    const res = { json: sinon.spy() };
+    const next = sinon.spy();
 
     // Act
-    import('../app').then((app) => {
-      chai.request(app.default)
-        .post('/api/animals')
-        .set('Content-Type', 'multipart/form-data')
-        .field('name', animalData.name)
-        .field('description', animalData.description)
-        .field('needs', animalData.needs)
-        .field('status', animalData.status)
-        .field('resume', animalData.resume)
-        .field('birthdate', animalData.birthdate)
-        .attach('photo1', fs.readFileSync(filePath), 'test_image.jpg')
-        .end((err, res) => {
-          // Assert
-          expect(res).to.have.status(200);
-          expect(res.body.name).to.equal(animalData.name);
-          expect(res.body.description).to.equal(animalData.description);
-          expect(res.body.needs).to.equal(animalData.needs);
-          expect(res.body.status).to.equal(animalData.status);
-          expect(res.body.resume).to.equal(animalData.resume);
-          expect(res.body.birthdate).to.equal(animalData.birthdate);
-          expect(res.body.photo1).to.match(/^[a-zA-Z0-9]+\.[a-z]+$/);
-          done();
-        });
-    });
+    await animalsController.getAll(null, res, next);
+
+    // Assert
+    expect(findAllStub.calledOnce).to.be.true;
+    expect(res.json.calledOnceWithExactly(fakeAnimals)).to.be.true;
+    expect(next.notCalled).to.be.true;
+
+    // Restore
+    findAllStub.restore();
+  });
+
+  it('should handle database errors', async () => {
+    // Arrange
+    const errorMessage = 'database error';
+    const findAllStub = sinon.stub(Animal, 'findAll').throws(new Error(errorMessage));
+    const res = { status: sinon.stub().returns({ json: sinon.spy() }) };
+    const next = sinon.spy();
+
+    // Act
+    await animalsController.getAll(null, res, next);
+
+    // Assert
+    expect(findAllStub.calledOnce).to.be.true;
+    expect(res.status.calledOnceWithExactly(500)).to.be.true;
+    expect(res.status().json.calledOnceWithExactly({ error: errorMessage })).to.be.true;
+    expect(next.notCalled).to.be.true;
+
+    // Restore
+    findAllStub.restore();
+  });
+
+  it('should handle empty result from database', async () => {
+    // Arrange
+    const findAllStub = sinon.stub(Animal, 'findAll').resolves(null);
+    const res = { json: sinon.spy() };
+    const next = sinon.spy();
+
+    // Act
+    await animalsController.getAll(null, res, next);
+
+    // Assert
+    expect(findAllStub.calledOnce).to.be.true;
+    expect(res.json.notCalled).to.be.true;
+    expect(next.calledOnce).to.be.true;
+    expect(next.args[0][0]).to.be.an.instanceOf(Error);
+    expect(next.args[0][0].message).to.equal('Problème de BDD');
+
+    // Restore
+    findAllStub.restore();
   });
 });
