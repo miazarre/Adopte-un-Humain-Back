9c729f21361366b0dce1e2e79eca25574f26a8b6
roissi <cyrildegraeve@gmail.com>
2023-03-10 10:49:28 +0100

    jest test with supertest

--- test/route_GETusers.test3.js
+++ test/route_GETusers.test3.js
@@ -5,9 +5,6 @@ require('@babel/register')({
 import request from 'supertest';
 import express from 'express';
 import User from '../app/models/user.js';
-import dbClient from '../app/service/dbClient.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
 // Création de l'application Express
 const app = express();
@@ -25,25 +22,39 @@ app.get('/users', async (req, res, next) => {
 // Définit un groupe de tests pour la méthode `findAll` du Model user
 describe('findAll', () => {
 
+  // Réinitialise les mocks avant chaque test
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   // Teste si la méthode `findAll` renvoie tous les utilisateurs existants de la base de données, si elle est exécutée correctement
   it('devrait retourner tous les users', async () => {
+    // Crée un objet `mockUsers` qui simule deux utilisateurs de la base de données
+    const mockUsers = [
+      { id: 1, firstname: 'Matt', lastname: 'Skrzypczak', email: 'matt@example.com', phone: '123456789', password: 'password1', address: '1 rue des Impasses', city: 'Miramas', country: 'France', role_id: 2 },
+      { id: 2, firstname: 'Cy', lastname: 'De Graeve', email: 'cy@example.com', phone: '987654321', password: 'password2', address: '2 impasse de la Rue', city: 'Paris', country: 'France', role_id: 1 }
+    ];
+
+    // Espionne la méthode `findAll` du modèle d'utilisateur et la configure pour qu'elle renvoie la liste `mockUsers`
+    jest.spyOn(User, 'findAll').mockResolvedValue(mockUsers);
+
     // Test de la requête HTTP GET '/users'
     const res = await request(app).get('/users');
 
     // Vérifie que le code de statut de la réponse est 200
     expect(res.status).toBe(200);
 
-    // Vérifie que la réponse contient au moins un utilisateur
-    expect(res.body.length).toBeGreaterThan(0);
+    // Vérifie que le corps de la réponse est égal à la liste `mockUsers`
+    expect(res.body).toEqual(mockUsers);
+
+    // Nettoie le mock
+    User.findAll.mockRestore();
   });
 
   // Teste si la méthode `findAll` affiche une erreur quand il y a un problème avec la base de données
   it('devrait afficher une erreur quand il y a un problème avec la BDD', async () => {
-    // Sauvegarde la méthode `query` originale
-    const originalQuery = dbClient.query;
-
-    // Espionne la méthode `query` et la configure pour qu'elle renvoie une erreur
-    dbClient.query = jest.fn().mockRejectedValue(new Error('Erreur BDD'));
+    // Espionne la méthode `findAll` du modèle d'utilisateur et la configure pour qu'elle renvoie une erreur
+    jest.spyOn(User, 'findAll').mockRejectedValue(new Error('Erreur BDD'));
 
     // Test de la requête HTTP GET '/users'
     const res = await request(app).get('/users');
@@ -51,7 +62,7 @@ describe('findAll', () => {
     // Vérifie que le code de statut de la réponse est 500
     expect(res.status).toBe(500);
 
-    // Rétablit la méthode `query` originale
-    dbClient.query = originalQuery;
+    // Nettoie le mock
+    User.findAll.mockRestore();
   });
 });
